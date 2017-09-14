c30eacfb15ac22bacec66bb84b4f44dab4ebb428
Dafydd <25325299+DafsCode@users.noreply.github.com>
2017-09-14 11:57:29 +0100

    Initial commit
    Contains all the code n stuff

---
+++ cat.js
@@ -0,0 +1,21 @@
+const snekfetch = require('snekfetch');
+const fs = require('fs');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('Ready to rumble!');
+});
+client.on('message', msg => {
+    if (msg.content === '+cat') {
+        snekfetch.get('http://lorempixel.com/500/500/cats/')
+        .then(r => fs.writeFile('download.jpg', r.body));
+        msg.channel.send("Here you go", {
+            file: "download.jpg"
+        });
+    }
+});
+client.on('message', msg => {
+    if (msg.content === '+help') {
+        msg.channel.send('There is one command. +cat. @Dafydd_someone')
+}});
+client.login('Token here')
